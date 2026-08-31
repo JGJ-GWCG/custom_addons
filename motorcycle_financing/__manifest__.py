7789424ff79e03f50d32fec46ee837a02ec89c2d
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2026-08-31 10:42:35 -0600

    Módulo de motorcycle agregado

---
+++ motorcycle_financing/__manifest__.py
@@ -0,0 +1,15 @@
+# Manifest Template. If you copy this template, make sure the indentation is right.
+{
+	'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+	'description': '',
+    'license': 'OPL-1',
+	'category': 'Kawiil/Custom Modules',
+	'author': 'JGJ-GWCG',
+	'website': 'https://github.com/odoo-trainings/kawiil_base.git',
+	'version': '18.0.0.0.1',
+	'depends': [],
+	'data': [],
+	'demo': [],
+	'application': 'True',
+}
