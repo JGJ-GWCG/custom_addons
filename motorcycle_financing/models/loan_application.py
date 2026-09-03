9dbbd74eea693fcd59590a7e6a48345c494272da
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2026-09-03 11:54:55 -0600

    Se modificó el modelo y la view para configurar la formm, la list y la search view

--- motorcycle_financing/models/loan_application.py
+++ motorcycle_financing/models/loan_application.py
@@ -5,15 +5,15 @@ class LoanModel(models.Model):
     _description = 'Loan Application'
 
     name = fields.Char( string = 'Application Number', required=True)
-    currency_id = fields.Many2one(string = 'Currency', required=True, comodel_name='res.currency')
-    date_application = fields.Date( string = 'Application Date')
-    date_approval = fields.Date( string = 'Approval Date')
-    date_rejection = fields.Date( string = 'Rejection Date')
-    date_signed = fields.Datetime( string = 'Signed on')
-    down_payment = fields.Monetary( string = 'Downpayment', currency_field='currency_id') #Relación con el campo de currency_id
-    interest_rate = fields.Float( string = 'Interest Rate (%)', digits = (5,4))
-    loan_amount = fields.Monetary( string = 'Loan Amount', currency_field='currency_id') #Relación con el campo de currency_id
+    currency_id = fields.Many2one(string = 'Currency', required=True, comodel_name='res.currency', default=lambda self:self.env.company.currency_id.id)
+    date_application = fields.Date( string = 'Application Date', readonly=True, copy=False)
+    date_approval = fields.Date( string = 'Approval Date', readonly=True, copy=False)
+    date_rejection = fields.Date( string = 'Rejection Date', readonly=True, copy=False)
+    date_signed = fields.Datetime( string = 'Signed on', readonly=True, copy=False)
+    down_payment = fields.Monetary( string = 'Downpayment', required=True, currency_field='currency_id') #Relación con el campo de currency_id
+    interest_rate = fields.Float( string = 'Interest Rate (%)', required=True, digits = (5,2))
+    loan_amount = fields.Monetary( string = 'Loan Amount', required=True, currency_field='currency_id') #Relación con el campo de currency_id
     loan_term = fields.Integer( string = 'Loan Term (Months)', required = True, default = 36)
-    rejection_reason = fields.Text( string = 'Rejection Reason')
-    state = fields.Selection (string = 'Status', selection=[('draft', 'Draft'), ('sent', 'Sent'), ('review', 'Credit Check'), ('approved', 'Approved'), ('rejected', 'Rejected'), ('signed', 'Signed'), ('cancel', 'Canceled')], default='draft')
-    notes = fields.Html (string = "Notes")
+    rejection_reason = fields.Text( string = 'Rejection Reason', copy=False)
+    state = fields.Selection (string = 'Status', selection=[('draft', 'Draft'), ('sent', 'Sent'), ('review', 'Credit Check'), ('approved', 'Approved'), ('rejected', 'Rejected'), ('signed', 'Signed'), ('cancel', 'Canceled')], default='draft', copy = False)
+    notes = fields.Html (string = "Notes", copy = False)
